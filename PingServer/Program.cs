ad0826f5903c78d2b55b471d1150252eeec38842
Jakmer <kubamerta13@gmail.com>
2024-10-28 00:21:51 +0100

    Call server in main program

--- PingServer/Program.cs
+++ PingServer/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+using MQTTnet.Samples.Server;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        await Server_Simple_Samples.Publish_Message_From_Broker();
+
+        Console.WriteLine("Press Enter to exit.");
+        Console.ReadLine();
+    }
+}
