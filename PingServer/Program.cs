fe45e082fbcfef601ba847ee768f1ca6a1fa8a2b
jakmer <kubamerta13@gmail.com>
2024-10-28 21:37:23 +0000

    Create initial server

--- PingServer/Program.cs
+++ PingServer/Program.cs
@@ -1,14 +1,1 @@
-﻿using System;
-using System.Threading.Tasks;
-using MQTTnet.Samples.Server;
-
-class Program
-{
-    static async Task Main(string[] args)
-    {
-        await Server_Simple_Samples.Publish_Message_From_Broker();
-
-        Console.WriteLine("Press Enter to exit.");
-        Console.ReadLine();
-    }
-}
+﻿await PingServer.Server.Run();
--- PingServer/Server.cs
+++ PingServer/Server.cs
@@ -1,210 +1,26 @@
+using MQTTnet;
 using MQTTnet.Diagnostics;
 using MQTTnet.Protocol;
 using MQTTnet.Server;
 
-namespace MQTTnet.Samples.Server;
-
-public static class Server_Simple_Samples
+namespace PingServer
 {
-    public static async Task Force_Disconnecting_Client()
+    public static class Server
     {
-        /*
-         * This sample will disconnect a client.
-         *
-         * See _Run_Minimal_Server_ for more information.
-         */
-
-        using (var mqttServer = await StartMqttServer())
+        public static async Task Run()
         {
-            // Let the client connect.
-            await Task.Delay(TimeSpan.FromSeconds(5));
+            var mqttFactory = new MqttFactory();
 
-            // Now disconnect the client (if connected).
-            var affectedClient = (await mqttServer.GetClientsAsync()).FirstOrDefault(c => c.Id == "MyClient");
-            if (affectedClient != null)
+            var mqttServerOptions = new MqttServerOptionsBuilder().WithDefaultEndpoint().Build();
+
+            using (var mqttServer = mqttFactory.CreateMqttServer(mqttServerOptions))
             {
-                await affectedClient.DisconnectAsync();
-            }
-        }
-    }
+                await mqttServer.StartAsync();
 
-    public static async Task Publish_Message_From_Broker()
-    {
-        /*
-         * This sample will publish a message directly at the broker.
-         *
-         * See _Run_Minimal_Server_ for more information.
-         */
+                Console.WriteLine("Press Enter to exit.");
+                Console.ReadLine();
 
-        using (var mqttServer = await StartMqttServer())
-        {
-            // Create a new message using the builder as usual
-            var message = new MqttApplicationMessageBuilder().WithTopic("HelloWorld").WithPayload("Test").Build();
-
-            // Now inject the new message at the broker.
-            await mqttServer.InjectApplicationMessage(
-                new InjectedMqttApplicationMessage(message)
-                {
-                    SenderClientId = "SenderClientId"
-                });
-        }
-    }
-
-    public static async Task Run_Minimal_Server()
-    {
-        /*
-         * This sample starts a simple MQTT server which will accept any TCP connection.
-         */
-
-        var mqttFactory = new MqttFactory();
-
-        // The port for the default endpoint is 1883.
-        // The default endpoint is NOT encrypted!
-        // Use the builder classes where possible.
-        var mqttServerOptions = new MqttServerOptionsBuilder().WithDefaultEndpoint().Build();
-
-        // The port can be changed using the following API (not used in this example).
-        // new MqttServerOptionsBuilder()
-        //     .WithDefaultEndpoint()
-        //     .WithDefaultEndpointPort(1234)
-        //     .Build();
-
-        using (var mqttServer = mqttFactory.CreateMqttServer(mqttServerOptions))
-        {
-            await mqttServer.StartAsync();
-
-            Console.WriteLine("Press Enter to exit.");
-            Console.ReadLine();
-
-            // Stop and dispose the MQTT server if it is no longer needed!
-            await mqttServer.StopAsync();
-        }
-    }
-
-    public static async Task Run_Server_With_Logging()
-    {
-        /*
-         * This sample starts a simple MQTT server and prints the logs to the output.
-         *
-         * IMPORTANT! Do not enable logging in live environment. It will decrease performance.
-         *
-         * See sample "Run_Minimal_Server" for more details.
-         */
-
-        var mqttFactory = new MqttFactory(new ConsoleLogger());
-
-        var mqttServerOptions = new MqttServerOptionsBuilder().WithDefaultEndpoint().Build();
-
-        using (var mqttServer = mqttFactory.CreateMqttServer(mqttServerOptions))
-        {
-            await mqttServer.StartAsync();
-
-            Console.WriteLine("Press Enter to exit.");
-            Console.ReadLine();
-
-            // Stop and dispose the MQTT server if it is no longer needed!
-            await mqttServer.StopAsync();
-        }
-    }
-
-    public static async Task Validating_Connections()
-    {
-        /*
-         * This sample starts a simple MQTT server which will check for valid credentials and client ID.
-         *
-         * See _Run_Minimal_Server_ for more information.
-         */
-
-        var mqttFactory = new MqttFactory();
-
-        var mqttServerOptions = new MqttServerOptionsBuilder().WithDefaultEndpoint().Build();
-
-        using (var mqttServer = mqttFactory.CreateMqttServer(mqttServerOptions))
-        {
-            // Setup connection validation before starting the server so that there is 
-            // no change to connect without valid credentials.
-            mqttServer.ValidatingConnectionAsync += e =>
-            {
-                if (e.ClientId != "ValidClientId")
-                {
-                    e.ReasonCode = MqttConnectReasonCode.ClientIdentifierNotValid;
-                }
-
-                if (e.UserName != "ValidUser")
-                {
-                    e.ReasonCode = MqttConnectReasonCode.BadUserNameOrPassword;
-                }
-
-                if (e.Password != "SecretPassword")
-                {
-                    e.ReasonCode = MqttConnectReasonCode.BadUserNameOrPassword;
-                }
-
-                return Task.CompletedTask;
-            };
-
-            await mqttServer.StartAsync();
-
-            Console.WriteLine("Press Enter to exit.");
-            Console.ReadLine();
-
-            await mqttServer.StopAsync();
-        }
-    }
-
-    static async Task<MqttServer> StartMqttServer()
-    {
-        var mqttFactory = new MqttFactory();
-
-        // Due to security reasons the "default" endpoint (which is unencrypted) is not enabled by default!
-        var mqttServerOptions = mqttFactory.CreateServerOptionsBuilder().WithDefaultEndpoint().Build();
-        var server = mqttFactory.CreateMqttServer(mqttServerOptions);
-        await server.StartAsync();
-        return server;
-    }
-
-    class ConsoleLogger : IMqttNetLogger
-    {
-        readonly object _consoleSyncRoot = new();
-
-        public bool IsEnabled => true;
-
-        public void Publish(MqttNetLogLevel logLevel, string source, string message, object[]? parameters, Exception? exception)
-        {
-            var foregroundColor = ConsoleColor.White;
-            switch (logLevel)
-            {
-                case MqttNetLogLevel.Verbose:
-                    foregroundColor = ConsoleColor.White;
-                    break;
-
-                case MqttNetLogLevel.Info:
-                    foregroundColor = ConsoleColor.Green;
-                    break;
-
-                case MqttNetLogLevel.Warning:
-                    foregroundColor = ConsoleColor.DarkYellow;
-                    break;
-
-                case MqttNetLogLevel.Error:
-                    foregroundColor = ConsoleColor.Red;
-                    break;
-            }
-
-            if (parameters?.Length > 0)
-            {
-                message = string.Format(message, parameters);
-            }
-
-            lock (_consoleSyncRoot)
-            {
-                Console.ForegroundColor = foregroundColor;
-                Console.WriteLine(message);
-
-                if (exception != null)
-                {
-                    Console.WriteLine(exception);
-                }
+                await mqttServer.StopAsync();
             }
         }
     }
