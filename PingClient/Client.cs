826c48d5068841990e32522a94f3b5d01c6d8254
Jakub <114589272+Jakmer@users.noreply.github.com>
2024-11-04 16:53:24 +0100

    Enable trivial communication between clients (#10)

--- PingClient/Client.cs
+++ PingClient/Client.cs
@@ -1,6 +1,8 @@
 using MQTTnet;
 using MQTTnet.Client;
 using MQTTnet.Formatter;
+using MQTTnet.Protocol;
+using System.Text;
 
 namespace PingClient
 {
@@ -14,12 +16,61 @@ namespace PingClient
             {
                 var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer("localhost", 1883).WithProtocolVersion(MqttProtocolVersion.V500).Build();
 
-                var response = await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+                await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
 
                 Console.WriteLine("The MQTT client is connected.");
 
+                _ = ReceiveMessagesAsync(mqttClient);
+
+                await SendMessageAsync(mqttClient);
+
                 await mqttClient.DisconnectAsync(new MqttClientDisconnectOptionsBuilder().WithReason(MqttClientDisconnectOptionsReason.NormalDisconnection).Build());
             }
+        }
+
+        private static async Task SendMessageAsync(IMqttClient mqttClient)
+        {
+            while (true)
+            {
+                Console.Write("Enter the message to send (or type 'exit' to quit): ");
+                string message = Console.ReadLine() ?? string.Empty;
+
+                if (message?.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                Console.Write("Enter the recipient ID: ");
+                string recipientId = Console.ReadLine() ?? string.Empty;
+
+                string topic = $"recipient/{recipientId}";
+
+                var mqttMessage = new MqttApplicationMessageBuilder()
+                    .WithTopic(topic)
+                    .WithPayload(message)
+                    .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.ExactlyOnce)
+                    .WithRetainFlag(false)
+                    .Build();
+
+                await mqttClient.PublishAsync(mqttMessage, CancellationToken.None);
+                Console.WriteLine($"Message sent to topic '{topic}': {message}");
+            }
+        }
+
+        private static async Task ReceiveMessagesAsync(IMqttClient mqttClient)
+        {
+            mqttClient.ApplicationMessageReceivedAsync += async e =>
+            {
+                var topic = e.ApplicationMessage.Topic;
+                var payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                Console.WriteLine($"Received message from topic '{topic}': {payload}");
+                await Task.CompletedTask;
+            };
+
+            // For now subscribe all topics
+            // TODO: Resolve problem with receiving self-send messages
+            await mqttClient.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic("#").Build());
+            Console.WriteLine("Subscribed to all topics.");
         }
     }
 }
