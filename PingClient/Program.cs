cbc135ca06a5eea6d324532ebfcb821037bc260d
kallazz <michael.w.kallas@gmail.com>
2024-11-04 18:10:47 +0100

    Reformat Client and improve message sending

--- PingClient/Program.cs
+++ PingClient/Program.cs
@@ -1,1 +1,34 @@
-﻿await PingClient.Client.ConnectAndDisconnectClientUsingMQTTv5();
+﻿
+Console.Write("Enter your user ID: ");
+var userId = Console.ReadLine();
+if (string.IsNullOrEmpty(userId))
+{
+    Console.Write("Id was not provided");
+    return 1;
+}
+
+var mqttClient = new PingClient.Client(userId);
+await mqttClient.Connect();
+await mqttClient.ReceiveMessages();
+
+while (true)
+{
+    Console.Write("Enter the message to send (or type 'exit' to quit): ");
+    var message = Console.ReadLine() ?? string.Empty;
+
+    if (message.ToLower() == "exit")
+    {
+        break;
+    }
+
+    Console.Write("Enter the recipient ID: ");
+    var recipientId = Console.ReadLine() ?? string.Empty;
+
+    await mqttClient.SendMessage(message, recipientId);
+
+    Console.WriteLine($"Message sent to user '{recipientId}': {message}");
+}
+
+await mqttClient.Disconnect();
+
+return 0;
