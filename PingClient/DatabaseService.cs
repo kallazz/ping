b65db8a1c5407698987e29e8cd10ee2ab4e79387
kallazz <michael.w.kallas@gmail.com>
2024-11-17 20:44:49 +0100

    Add login

--- PingClient/DatabaseService.cs
+++ PingClient/DatabaseService.cs
@@ -32,5 +32,40 @@ namespace PingClient
 
             return true;
         }
+
+        public async Task<string?> GetPasswordForUserByUsername(string username)
+        {
+            return await GetPasswordForUser("username", username);
+        }
+
+        public async Task<string?> GetPasswordForUserByEmail(string email)
+        {
+            return await GetPasswordForUser("email", email);
+        }
+
+        private async Task<string?> GetPasswordForUser(string fieldName, string value)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                {
+                    await connection.OpenAsync();
+
+                    string query = $"SELECT password FROM users WHERE {fieldName} = @Value";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Value", value);
+
+                        object? result = await command.ExecuteScalarAsync();
+
+                        return result?.ToString();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
     }
 }
